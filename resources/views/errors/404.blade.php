a6ee06a2f93855c7f33e81e2187a2033c68d4c4f
Yu Li <liyu001989@gmail.com>
2015-04-01 21:22:43 +0800

    提交框架

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<title>resource not found</title>
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				width: 100%;
+				height: 100%;
+				color: #B0BEC5;
+				display: table;
+				font-weight: 100;
+			}
+
+			.container {
+				text-align: center;
+				display: table-cell;
+				vertical-align: middle;
+			}
+
+			.content {
+				text-align: center;
+				display: inline-block;
+			}
+
+			.title {
+				font-size: 72px;
+				margin-bottom: 40px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="content">
+				<div class="title">The requested resource is not found.</div>
+			</div>
+		</div>
+	</body>
+</html>
